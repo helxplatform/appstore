421cb0d8c14c66c2b8d17b3b9f10b7442fc5982a
Isma Gilani <igilani@renci.org>
2020-04-20 13:33:27 -0400

    clean up git repo

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\muralikarthik.k\pycharmprojects\test_allauth\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
