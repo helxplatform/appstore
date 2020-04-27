0b46879904408d739064e8af9ae297130c71b737
Isma Gilani <igilani@renci.org>
2020-04-27 12:26:50 -0400

    users db plus comment out logger statements from middleware filter

--- CS_AppsStore/middleware/filter_whitelist_middleware.py
+++ CS_AppsStore/middleware/filter_whitelist_middleware.py
@@ -3,14 +3,14 @@ from django.conf import settings
 from django.utils.deprecation import MiddlewareMixin
 
 import logging
-logger = logging.getLogger (__name__)
-FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
-logging.basicConfig(format=FORMAT)
+#logger = logging.getLogger (__name__)
+#FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
+#logging.basicConfig(format=FORMAT)
 
 class AllowWhiteListedUserOnly(MiddlewareMixin):
     def process_request(self, request):
         user = request.user
-        logger.info (f"testing user: {user}")
+        #logger.info (f"testing user: {user}")
         print(f"testing user: {user}")
 
         if user.is_authenticated and not user.is_superuser:
@@ -18,11 +18,11 @@ class AllowWhiteListedUserOnly(MiddlewareMixin):
                     and not request.path.startswith(settings.ADMIN_URL) \
                     and not request.path.startswith(settings.STATIC_URL):
                 if not user.groups.filter(name='whitelisted').exists():
-                    logger.info (f"user groups for user {user}: {user.groups}")
-                    logger.info (f"Filtering user {user} is not whitelisted")
+                    #logger.info (f"user groups for user {user}: {user.groups}")
+                    #logger.info (f"Filtering user {user} is not whitelisted")
                     print (f"user groups for user {user}: {user.groups}")
                     print (f"Filtering user {user} is not whitelisted")
                     return HttpResponseRedirect(settings.LOGIN_URL)
-        logger.info (f"accepting user {user}")
+        #logger.info (f"accepting user {user}")
         print(f"accepting user {user}")
         return None
