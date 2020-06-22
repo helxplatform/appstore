20f74d7e21923e79376479ae4dd8ba886b48b778
Isma Gilani <igilani@renci.org>
2020-06-22 16:32:56 -0400

    django_saml2 integration

--- appstore/appstore/settings/base.py
+++ appstore/appstore/settings/base.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.github',
     'allauth.socialaccount.providers.google',
-    'bootstrapform'
+    'bootstrapform',
+    'django_saml2_auth'
 ]
 
 SITE_ID = 4
@@ -182,6 +183,9 @@ LOGIN_URL = '/accounts/login'
 ADMIN_URL = '/admin'
 LOGIN_WHITELIST_URL = '/login_whitelist/'
 
+SAML_URL = '/accounts/saml'
+SAML_ACS_URL = '/saml2_auth/acs/'
+
 REST_USE_JWT = True
 
 DEFAULT_AUTHENTICATION_CLASSES = [
@@ -272,4 +276,26 @@ LOGGING = {
             'level': 'DEBUG',
         },
     },
+}
+
+SAML2_AUTH = {
+    # Metadata is required, choose either remote url or local file path
+    'METADATA_AUTO_CONF_URL': 'https://sso.unc.edu/metadata/unc',
+
+    # Optional settings below
+    'DEFAULT_NEXT_URL': '/apps/',  # Custom target redirect URL after the user get logged in. Default to /admin if not set. This setting will be overwritten if you have parameter ?next= specificed in the login URL.
+    'CREATE_USER': 'TRUE', # Create a new Django user when a new user logs in. Defaults to True.
+    'NEW_USER_PROFILE': {
+        'USER_GROUPS': [],  # The default group name when a new user logs in
+        'ACTIVE_STATUS': True,  # The default active status for new users
+        'STAFF_STATUS': True,  # The staff status for new users
+        'SUPERUSER_STATUS': False,  # The superuser status for new users
+    },
+    'ATTRIBUTES_MAP': {  # Change Email/UserName/FirstName/LastName to corresponding SAML2 userprofile attributes.
+        'email': 'email',
+        'username': 'uid',
+        'first_name': 'givenName',
+        'last_name': 'surname',
+    },
+    'ENTITY_ID': 'https://restartingresearch.renci.org/shibboleth', # Populates the Issuer element in authn request
 }
