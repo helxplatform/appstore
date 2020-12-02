079d5a241ad7d764e854610a7092cf253af867e6
Isma Gilani <igilani@renci.org>
2020-12-02 16:00:12 -0500

    fix host url in email body

--- appstore/appstore/forms.py
+++ appstore/appstore/forms.py
@@ -12,11 +12,16 @@ class CustomSignupForm(SignupForm):
     def save(self, request):
         user = super(CustomSignupForm, self).save(request)
 
+        print('sending email')
+
+        msg2 = 'test'
+        msg = 'A new user ' + user.email + ' has signed up for access to ' + settings.APPLICATION_BRAND \
+            + ' and needs to be reviewed for whitelisting. Upon successful review, kindly add the user to' \
+            + ' Authorized Users on django admin panel on ' \
+            + request.META['SERVER_NAME'] + ':' + request.META['SERVER_PORT'] + '/' + settings.ADMIN_URL + '.'
         send_mail(
             'Whitelisting Required',
-            'A new user ' + user.email + ' has signed up for access to ' + settings.APPLICATION_BRAND
-            + ' and needs to be reviewed for whitelisting. Upon successful review, kindly add the user to Authorized Users on '
-            + settings.SITE_URL + settings.ADMIN_URL + '.',
+            msg,
             settings.EMAIL_HOST_USER,
             [settings.APPLICATION_BRAND + '-admin@lists.renci.org'],
             fail_silently=False,
