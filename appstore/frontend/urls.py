634d219fb779c74568017559aa35e13e43484af6
Alexander Hagerman <ahagerman@renci.org>
2021-03-16 16:38:32 -0400

    Correct url path in frontend

--- appstore/frontend/urls.py
+++ appstore/frontend/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import FrontendView
 
 urlpatterns = [
-    path('frontend', FrontendView.as_view(), name='frontend'),
+    path('frontend/', FrontendView.as_view(), name='frontend'),
 ]
