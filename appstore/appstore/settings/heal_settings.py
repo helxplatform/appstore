4c11254dc25e6700d632af0747d8fe4882f61ebf
Isma Gilani <igilani@renci.org>
2021-06-11 11:00:42 -0400

    update heal logo

--- appstore/appstore/settings/heal_settings.py
+++ appstore/appstore/settings/heal_settings.py
@@ -6,7 +6,7 @@ APPLICATION_BRAND = "heal"
 PRODUCT_SETTINGS = ProductSettings(
     brand="heal",
     title="NIH HEAL Initiative",
-    logo_url="/static/images/heal/heal-social-logo.png",
+    logo_url="/static/images/heal/logo.png",
     color_scheme=ProductColorScheme("#8a5a91", "#505057"),
     links=None,
 )
