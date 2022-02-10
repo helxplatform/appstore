8fc50271242665691ee92a3514eb051e13e8b6ed
Griffin Roupe <groupe@renci.org>
2022-02-10 12:48:23 -0500

    Add artillery testing infrastructure & test for login -> create app

---
+++ artillery-tests/helpers.js
@@ -0,0 +1,62 @@
+/**
+ * Parse an array of Set-Cookie values into a simple cookieName -> cookieValue map.
+ * 
+ * @param {string[]} header - Set-Cookie header values (without Set-Cookie: prefixes). As in response.headers['set-cookie'].
+ */
+function simpleParseSetCookies(header) {
+    if (typeof header === "undefined") return {};
+    return header
+        // Set-Cookie always begins with <cookie-name>=<cookie-value>; where the hanging semicolon is optional
+        .map((cookie) => cookie.split(";")[0].split("="))
+        .reduce((acc, cur) => {
+            const [ name, value ] = cur;
+            acc[name] = value;
+            return acc;
+        }, {});
+}
+function updateSession(requestParams, response, context, ee, next) {
+    const cookies = simpleParseSetCookies(response.headers["set-cookie"]);
+    // if (typeof context.vars["csrf_token"] === "undefined") context.vars["csrf_token"] = "";
+    if (cookies.hasOwnProperty("sessionid")) {
+        context.vars["session_id"] = cookies["sessionid"];
+        console.log("Update sess id");
+    }
+    if (cookies.hasOwnProperty("csrftoken")) {
+        context.vars["csrf_token"] = cookies["csrftoken"];
+        console.log("Update csrf");
+    }
+    return next();
+}
+function countApps(requestParams, response, context, ee, next) {
+    // Set app_count context variable from GET /api/v1/instances/
+    if (!context.vars.hasOwnProperty("app_count")) context.vars["app_count"] = [];
+    context.vars["app_count"].push(JSON.parse(response.body).length);
+    console.log(context.vars["app_count"]);
+    return next();
+}
+function disableCookies(requestParams, context, ee, next) {
+    context._enableCookieJar = false;
+    if (requestParams.cookieJar) requestParams.cookieJar.removeAllCookiesSync();
+    return next();
+}
+function parseInitialApps(requestParams, response, context, ee, next) {
+    const apps = JSON.parse(response.body);
+    context.vars["apps"] = apps;
+    console.log(apps.length);
+    return next();
+}
+function parseNewApps(requestParams, response, context, ee, next) {
+    const apps = JSON.parse(response.body);
+
+    const oldAppIds = context.vars["apps"].map((app) => app.sid);
+    const spawnedApp = apps.filter((app) => !oldAppIds.includes(app.sid))[0];
+    context.vars["spawned_app"] = spawnedApp;
+    return next();
+}
+module.exports = {
+    disableCookies,
+    updateSession,
+    countApps,
+    parseInitialApps,
+    parseNewApps
+};
---
+++ artillery-tests/node_modules/@artilleryio/sketches-js/README.md
@@ -0,0 +1,6 @@
+# sketches-js
+
+This is a fork of [@datadog/sketches-js](https://www.npmjs.com/package/@datadog/sketches-js) used by [Artillery](https://www.artillery.io) with a couple of fixes that haven't been upstreamed yet:
+
+- Retain summary statistics on merge - [PR #13](https://github.com/DataDog/sketches-js/pull/13)
+- Set `zeroCount` when on merge - [PR #18](https://github.com/DataDog/sketches-js/pull/18)
