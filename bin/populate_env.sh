3f1bdc505a5e50f29380e1a7fa17106b171586a9
Griffin Roupe <groupe@renci.org>
2022-06-22 12:03:23 -0400

    Update populate_env : analytics now can be disabled, deployment namespace is exposed to UI via env.json.

--- bin/populate_env.sh
+++ bin/populate_env.sh
@@ -12,7 +12,7 @@ search_url="${REACT_APP_HELX_SEARCH_URL}"
 brand_name="${REACT_APP_UI_BRAND_NAME}"
 tranql_enabled="${REACT_APP_TRANQL_ENABLED:-false}"
 tranql_url="${REACT_APP_TRANQL_URL:-\/tranql}"
-analytics="${REACT_APP_ANALYTICS}"
+analytics="${REACT_APP_ANALYTICS:-}"
 hidden_support_sections="${REACT_APP_HIDDEN_SUPPORT_SECTIONS}"
 deployment_namespace="${REACT_APP_DEPLOYMENT_NAMESPACE}"
 
@@ -33,17 +33,18 @@ template='{
       "ga_property": ""
       }
     },
-    "hidden_support_sections": "%HIDDEN_SUPPORT_SECTIONS%"
+    "hidden_support_sections": "%HIDDEN_SUPPORT_SECTIONS%",
+    "deployment_namespace": "%DEPLOYMENT_NAMESPACE%"
 }'
 
 echo $template | sed \
-  -e "s+%WORKSPACES_ENABLED%+$workspaces_enabled+" \
-  -e "s+%SEARCH_ENABLED%+$search_enabled+" \
-  -e "s+%SEARCH_URL%+$search_url+" \
-  -e "s+%BRAND%+$brand_name+" \
-  -e "s+%HIDDEN_SUPPORT_SECTIONS%+$hidden_support_sections+" \
-  -e "s+%ANALYTICS%+$analytics+" \
-  -e "s+%TRANQL_ENABLED%+$tranql_enabled+" \
-  -e "s+%TRANQL_URL%+$tranql_url+" \
+  -e "s/%WORKSPACES_ENABLED%/$workspaces_enabled/" \
+  -e "s/%SEARCH_ENABLED%/$search_enabled/" \
+  -e "s/%SEARCH_URL%/$search_url/" \
+  -e "s/%BRAND%/$brand_name/" \
+  -e "s/%HIDDEN_SUPPORT_SECTIONS%/$hidden_support_sections/" \
+  -e "s/%ANALYTICS%/$analytics/" \
+  -e "s/%TRANQL_ENABLED%/$tranql_enabled/" \
+  -e "s/%TRANQL_URL%/$tranql_url/" \
   -e "s/%DEPLOYMENT_NAMESPACE%/$deployment_namespace/" \
   > $1
