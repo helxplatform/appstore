aa138ce15cf1db0cd625152e27959103f1de8725
PJ Linebaugh <pjl@renci.org>
2022-12-16 13:05:15 -0500

    adding eduhelx-chip690 settings file

---
+++ appstore/appstore/settings/eduhelx-chip690_settings.py
@@ -0,0 +1,14 @@
+from .base import *
+from product.configuration import ProductSettings, ProductColorScheme, ProductLink
+
+# TODO remove Application brand once the new frontend is complete and
+# the django templates in core are removed.
+APPLICATION_BRAND = "eduhelx-chip690"
+
+PRODUCT_SETTINGS = ProductSettings(
+  brand="chip690",
+  title="EduHeLx CHIP690",
+  logo_url="/static/images/eduhelx/logo.png",
+  color_scheme=ProductColorScheme("#666666", "#e6e6e6"), #TBD
+  links=[],
+)
