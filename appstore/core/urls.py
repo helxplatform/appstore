2abb7530f41e6b7a9b284902c12472d6f0bbe06b
waTeim <truthset@gmail.com>
2023-03-31 13:34:40 -0400

    fix: Add files so reverse(index) exists to avoid stack trace during stack trace processing

--- appstore/core/urls.py
+++ appstore/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import auth
+from .views import auth,index
 
 urlpatterns = [
+    path('default/', index, name='index'),
     path("auth/", auth, name="auth"),
 ]
