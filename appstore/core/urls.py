ed01f4648d4647e87299f91e3f74dd97da47e40c
muralikarthikk <muralikarthik.k123@gmail.com>
2023-04-21 09:56:21 -0400

    fix for Resolver404 stack traces

--- appstore/core/urls.py
+++ appstore/core/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-from .views import auth,index
+from django.urls import path, re_path
+from .views import auth, index, HandlePrivateURL404s
 
 urlpatterns = [
     path('default/', index, name='index'),
     path("auth/", auth, name="auth"),
+    re_path(r"^private/*", HandlePrivateURL404s, name="private"),
 ]
