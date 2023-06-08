a393b5f8db7fd6ad227e182ef410a2b712e37b6d
PJ Linebaugh <pjl@renci.org>
2023-06-08 19:08:09 -0400

    add comment

--- appstore/api/v1/serializers.py
+++ appstore/api/v1/serializers.py
@@ -57,6 +57,8 @@ class ResourceSerializer(serializers.Serializer):
     cpus = serializers.FloatField()
     gpus = serializers.IntegerField(default=0)
     memory = serializers.CharField(validators=[memory_format_validator])
+    # Can something like this if we add an ephemeral storage selector
+    # in the web UI.
     # ephemeralStorage = serializers.CharField(validators=[memory_format_validator])
 
     def create(self, validated_data):
