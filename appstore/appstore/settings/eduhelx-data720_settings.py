2ea81c74162cbe128c8b4211cf004948947894b9
Charles Bennett <cnbennett3@gmail.com>
2023-10-19 12:37:22 -0400

    Add settings for data720.

--- appstore/appstore/settings/eduhelx-data720_settings.py
+++ appstore/appstore/settings/eduhelx-data720_settings.py
@@ -3,11 +3,11 @@ from product.configuration import ProductSettings, ProductColorScheme, ProductLi
 
 # TODO remove Application brand once the new frontend is complete and
 # the django templates in core are removed.
-APPLICATION_BRAND = "eduhelx-dev"
+APPLICATION_BRAND = "eduhelx-data720"
 
 PRODUCT_SETTINGS = ProductSettings(
-    brand="eduhelx-dev",
-    title="EduHeLx Dev",
+    brand="eduhelx-data720",
+    title="EduHeLx Data720",
     logo_url="/static/images/eduhelx/logo.png",
     color_scheme=ProductColorScheme("#666666", "#e6e6e6"), #TBD
     links=[],
