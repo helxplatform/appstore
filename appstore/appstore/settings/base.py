b5cfe9ba00ca41fa0efe6ad96a6d4cecb7a89420
Charles Bennett <cnbennett3@gmail.com>
2024-01-26 10:27:26 -0500

    Make appstore changes to sync with IT email changes.

--- appstore/appstore/settings/base.py
+++ appstore/appstore/settings/base.py
@@ -236,12 +236,12 @@ STATICFILES_FINDERS = (
 
 # Email configuration
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = "587"
+EMAIL_HOST = "relay.unc.edu"
+EMAIL_PORT = "25"
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "appstore@renci.org")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 RECIPIENT_EMAILS = os.environ.get("RECIPIENT_EMAILS", "")
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
 DEFAULT_FROM_EMAIL = os.environ.get("APPSTORE_DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
 DEFAULT_SUPPORT_EMAIL = os.environ.get(
     "APPSTORE_DEFAULT_SUPPORT_EMAIL", EMAIL_HOST_USER
